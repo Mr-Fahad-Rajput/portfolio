2e96ff848c86adf9574a439bce3d99eb780a3e5c
Fahad Rajput <fahadameenrajput@gmail.com>
2023-07-31 15:54:26 +0500

    Routing Done, Navbar HMR not Working

--- Floatfolio/frontend/src/pages/components/Navbar/Navbar.jsx
+++ Floatfolio/frontend/src/pages/components/Navbar/Navbar.jsx
@@ -15,11 +15,11 @@ function Navbar() {
       <header className='flex flex-row fixed m-4 rounded-lg innerDiv overflow-auto'>
      <nav className=" flex flex-wrap items-center justify-between md:py-0 text-lg text-gray-800 bg-gray-100 rounded-lg ">
             {/* Logo */}
-            <div className="w-[20%] h-fill ">
+            <div className="w-[20%] h-fill m-1">
             <img className='btn' src={logo} alt="Faadii's Tech logo" aria-label='Faadii-logo' type="svg" />
             </div>
             {/* Responsive Menu SVG */}
-            <svg xmlns="http://www.w3.org/2000/svg" aria-label='Mobile Menu Button' onClick={handleMenuButtonClick} className="h-8 w-8 btn md:hidden block" fill="none" viewBox="0 0 24 24" stroke="currentColor" >
+            <svg xmlns="http://www.w3.org/2000/svg" aria-label='Mobile Menu Button' onClick={handleMenuButtonClick} className="h-8 w-8 btn md:hidden  m-1 bg-slate-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" >
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
        
@@ -42,9 +42,9 @@ function Navbar() {
             </li>
           </ul>
         </div>
-              <div className={`md:w-[20%] md:h-fill flex flex-row justify-between  ${!menuVisibilty ? 'max-md:w-[33%]' : 'max-md:w-full'}`}>
-              <button className='md:p-4 py-2  btn' aria-label='Sign In Button' >Sign In</button>
-              <button className='md:p-4 py-2  btn' aria-label='Sign Up Button'>Sign Up</button>
+              <div className={`md:w-[20%] md:h-fill flex flex-row ${!menuVisibilty ? 'justify-around' : 'justify-between'}  ${!menuVisibilty ? 'max-md:w-[33%]' : 'max-md:w-full'}`}>
+              <button className='md:p-4 py-2 m-1 btn' aria-label='Sign In Button' >Sign In</button>
+              <button className='md:p-4 py-2 m-1 btn' aria-label='Sign Up Button'>Sign Up</button>
               </div>
     </nav>
   </header>  
