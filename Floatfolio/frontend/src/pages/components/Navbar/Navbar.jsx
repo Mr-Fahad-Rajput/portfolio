0bf803ca4671196c5441bf3c801867a023ba259e
Fahad Rajput <fahadameenrajput@gmail.com>
2023-07-31 16:24:14 +0500

    Navbar Perfect

--- Floatfolio/frontend/src/pages/components/Navbar/Navbar.jsx
+++ Floatfolio/frontend/src/pages/components/Navbar/Navbar.jsx
@@ -43,8 +43,8 @@ function Navbar() {
           </ul>
         </div>
               <div className={`md:w-[20%] md:h-fill flex flex-row ${!menuVisibilty ? 'justify-around' : 'justify-between'}  ${!menuVisibilty ? 'max-md:w-[33%]' : 'max-md:w-full'}`}>
-              <button className='md:p-4 py-2 m-1 btn' aria-label='Sign In Button' >Sign-In</button>
-              <button className='md:p-4 py-2 m-1 btn' aria-label='Sign Up Button'>Sign-Up</button>
+              <button className='md:p-4 py-2 m-1 btn whitespace-nowrap ' aria-label='Sign In Button' >Sign In</button>
+              <button className='md:p-4 py-2 m-1 btn whitespace-nowrap' aria-label='Sign Up Button'>Sign Up</button>
               </div>
     </nav>
   </header>  
