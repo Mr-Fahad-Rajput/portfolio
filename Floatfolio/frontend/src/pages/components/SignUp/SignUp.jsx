c0fbf09e35a938dd8afdd9adb58f6eba3a3c4e77
Fahad Rajput <fahadameenrajput@gmail.com>
2023-08-03 18:40:19 +0500

    Signup complete Functionality
    Signup design, Back navigation added to click outside the box

--- Floatfolio/frontend/src/pages/components/SignUp/SignUp.jsx
+++ Floatfolio/frontend/src/pages/components/SignUp/SignUp.jsx
@@ -1,16 +1,21 @@
 import './SignUp.css';
+import {useNavigate} from "react-router-dom";
 
 function SignUp() {
+  const history = useNavigate();
+  function goBack(){
+    history(-1);
+  }
     
     
   return (
     <>
-     <section className="position-relative">
-      <div className="absolute inset-0 bg-black opacity-50 "></div>
-        <div className="container-fluid relative">
+     <section className="">
+      <div className="absolute inset-0 bg-black opacity-50 z-10" onClick={goBack}></div>
+        <div className=" relative">
               {/* Start Content */}
-              <div className="text-center m-auto">
-                <div className="w-full max-w-sm m-auto p-7 div whitespace-nowrap">
+              <div className="text-center m-auto ">
+                <div className="relative w-full max-w-sm m-auto p-7 div whitespace-nowrap z-20">
                     <h5 className="mb-8 text-xl dark:text-white font-semibold underline">
                       SignUp
                     </h5>
@@ -18,20 +23,20 @@ function SignUp() {
                       <div className="grid grid-cols-1">
                         <div className="mb-4">
                           <label className="dark:text-white mr-4" htmlFor="RegisterName" > Name:</label>
-                          <input id="RegisterName" type="text"  className="customFormInput form-input  md:p-4 py-2 m-1 rounded-lg " placeholder="Full Name"/>
+                          <input id="RegisterName" type="text"  className=" md:p-4 py-2 m-1 rounded-lg " placeholder="Full Name"/>
                         </div>
                         <div className="mb-4">
                           <label className="dark:text-white mr-4" htmlFor="RegisterEmail" > Email: </label>
-                          <input id="RegisterEmail" type="email"  className="customFormInput form-input md:p-4 py-2 m-1 rounded-lg " placeholder="name@example.com"/>
+                          <input id="RegisterEmail" type="email"  className="md:p-4 py-2 m-1 rounded-lg " placeholder="name@example.com"/>
                         </div>
                         <div className="mb-4">
                           <label className="dark:text-white" htmlFor="RegisterPassword" >Password:</label>
-                          <input id="RegisterPassword" type="password" className="customFormInput form-input md:p-4 py-2 m-1 rounded-lg" placeholder="Something Strong"/>
+                          <input id="RegisterPassword" type="password" className=" md:p-4 py-2 m-1 rounded-lg" placeholder="Something Strong"/>
                         </div>
                         <div className="mb-4">
                           <div className="w-full"> 
-                          <input className="form-checkbox rounded w-4 h-4 me-2 border border-inherit" type="checkbox" value="" id="AcceptT&C"/> 
-                          <label className="form-check-label text-slate-400" htmlFor="AcceptT&C">   I Accept{" "}   
+                          <input className=" rounded w-4 h-4 me-2 border border-inherit" type="checkbox" value="" id="AcceptT&C"/> 
+                          <label className=" text-slate-400" htmlFor="AcceptT&C">   I Accept{" "}   
                           <a href="" className="text-balBrand">     Terms And Condition   </a> 
                           </label>
                           </div>
@@ -42,7 +47,7 @@ function SignUp() {
                         <div className="text-center">
                           <span className="text-slate-400 dark:text-slate-300 me-2"> Already have an account ?{" "}
                           </span>{" "}
-                          <a href="signin" className="text-dark dark:text-white fw-bold"> Sign in</a>
+                          <a href="signin" className=" dark:text-white fw-bold"> Sign in</a>
                         </div>
                       </div>
                     </form>
