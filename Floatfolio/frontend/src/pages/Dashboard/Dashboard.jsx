45795677560983ed3e7f26ec8209d5753f404a19
Fahad Rajput <fahadameenrajput@gmail.com>
2023-08-17 11:17:02 +0500

    two charts added to dash

--- Floatfolio/frontend/src/pages/Dashboard/Dashboard.jsx
+++ Floatfolio/frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,37 +1,53 @@
-import CardOne from '../components/CardOne.jsx';
-import viewIcon from './views.svg';
-import userIcon from './users.svg';
-import salesIcon from './sales.svg';
-import profitIcon from './dollar.svg';
-
+import CardOne from "../components/CardOne.jsx";
+import ChartOne from "../components/ChartOne.jsx";
+import ChartTwo from "../components/ChartTwo.jsx";
+import viewIcon from "./views.svg";
+import userIcon from "./users.svg";
+import salesIcon from "./sales.svg";
+import profitIcon from "./dollar.svg";
 
 function Dashboard() {
-    return (
-      <>
-        <section className="mainContent">
+  return (
+    <>
+      <section className="mainContent">
         <h1 className="mb-4 dark:text-secondaryBg font-semibold text-2xl underline cursor-default text-balBrand border-y-2 dark:border-mainBg  border-dBrand">
-                Monthly Report
+          Monthly Report
         </h1>
         <div className="grid rounded-lg m-2 grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-8 bg-secondaryBg dark:bg-balBrand">
-        <CardOne cardImg={viewIcon} cardHeading="Visits:353280" subHeading="Growth:" difPercntage="1.33%"/>
-        <CardOne cardImg={salesIcon} cardHeading="Sales: $25040" subHeading="Growth:" difPercntage="0.46%"/>
-        <CardOne cardImg={profitIcon} cardHeading="Profits:$4560" subHeading="Growth:" difPercntage="2.89%"/>
-        <CardOne cardImg={userIcon} cardHeading="Users:65490" subHeading="Growth:" difPercntage="10.09%"/>
-      </div>
-{/* eslint-disable */}
-      {/* <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
-        <ChartOne />
-        <ChartTwo />
-        <ChartThree />
-        <MapOne />
-        <div className="col-span-12 xl:col-span-8">
-          <TableOne />
+          <CardOne
+            cardImg={viewIcon}
+            cardHeading="Visits:353280"
+            subHeading="Growth:"
+            difPercntage="1.33%"
+          />
+          <CardOne
+            cardImg={salesIcon}
+            cardHeading="Sales: $25040"
+            subHeading="Growth:"
+            difPercntage="0.46%"
+          />
+          <CardOne
+            cardImg={profitIcon}
+            cardHeading="Profits:$4560"
+            subHeading="Growth:"
+            difPercntage="2.89%"
+          />
+          <CardOne
+            cardImg={userIcon}
+            cardHeading="Users:65490"
+            subHeading="Growth:"
+            difPercntage="10.09%"
+          />
         </div>
-        <ChatCard />
-      </div> */}
-        </section>
-      </>
-    );
-  }
-  export default Dashboard;
-  
+        <div className="p-8 w-full flex flex-row">
+          <ChartOne />
+          <ChartTwo />
+        </div>
+          {/* <ChartThree /> */}
+          {/* <div className=""><TableOne /></div> */}
+          {/* <ChatCard /> */}
+      </section>
+    </>
+  );
+}
+export default Dashboard;
--- Floatfolio/frontend/src/pages/components/ChartOne.jsx
+++ Floatfolio/frontend/src/pages/components/ChartOne.jsx
@@ -1,21 +1,20 @@
-import { ApexOptions } from 'apexcharts';
-import React, { useState } from 'react';
-import ReactApexChart from 'react-apexcharts';
+import React, { useState } from "react";
+import ReactApexChart from "react-apexcharts";
 
-const options: ApexOptions = {
+const options = {
   legend: {
     show: false,
-    position: 'top',
-    horizontalAlign: 'left',
+    position: "top",
+    horizontalAlign: "left",
   },
-  colors: ['#3C50E0', '#80CAEE'],
+  colors: ["#3C50E0", "#80CAEE"],
   chart: {
-    fontFamily: 'Satoshi, sans-serif',
+    fontFamily: "Satoshi, sans-serif",
     height: 335,
-    type: 'area',
+    type: "area",
     dropShadow: {
       enabled: true,
-      color: '#623CEA14',
+      color: "#623CEA14",
       top: 10,
       blur: 4,
       left: 0,
@@ -46,7 +45,7 @@ const options: ApexOptions = {
   ],
   stroke: {
     width: [2, 2],
-    curve: 'straight',
+    curve: "straight",
   },
   // labels: {
   //   show: false,
@@ -69,8 +68,8 @@ const options: ApexOptions = {
   },
   markers: {
     size: 4,
-    colors: '#fff',
-    strokeColors: ['#3056D3', '#80CAEE'],
+    colors: "#fff",
+    strokeColors: ["#3056D3", "#80CAEE"],
     strokeWidth: 3,
     strokeOpacity: 0.9,
     strokeDashArray: 0,
@@ -82,20 +81,20 @@ const options: ApexOptions = {
     },
   },
   xaxis: {
-    type: 'category',
+    type: "category",
     categories: [
-      'Sep',
-      'Oct',
-      'Nov',
-      'Dec',
-      'Jan',
-      'Feb',
-      'Mar',
-      'Apr',
-      'May',
-      'Jun',
-      'Jul',
-      'Aug',
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
     ],
     axisBorder: {
       show: false,
@@ -107,7 +106,7 @@ const options: ApexOptions = {
   yaxis: {
     title: {
       style: {
-        fontSize: '0px',
+        fontSize: "0px",
       },
     },
     min: 0,
@@ -115,74 +114,51 @@ const options: ApexOptions = {
   },
 };
 
-interface ChartOneState {
-  series: {
-    name: string;
-    data: number[];
-  }[];
-}
-
-const ChartOne: React.FC = () => {
-  const [state, setState] = useState<ChartOneState>({
+const ChartOne = () => {
+  const [state, setState] = useState({
     series: [
       {
-        name: 'Product One',
+        name: "Product One",
         data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30, 45],
       },
 
       {
-        name: 'Product Two',
+        name: "Product Two",
         data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39, 51],
       },
     ],
   });
 
   return (
-    <div className="col-span-12 rounded-sm border border-stroke bg-white px-5 pt-7.5 pb-5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:col-span-8">
-      <div className="flex flex-wrap items-start justify-between gap-3 sm:flex-nowrap">
-        <div className="flex w-full flex-wrap gap-3 sm:gap-5">
-          <div className="flex min-w-47.5">
-            <span className="mt-1 mr-2 flex h-4 w-full max-w-4 items-center justify-center rounded-full border border-primary">
-              <span className="block h-2.5 w-full max-w-2.5 rounded-full bg-primary"></span>
-            </span>
+    <div className=" rounded-lg border m-2 bg-secondaryBg  dark:border-strokedark dark:bg-boxdark overflow-hidden w-[75%]">
+      <div className="flex-col">
+        <div className=" w-auto h-2/3 flex flex-row justify-evenly text-dBrand">
+          <div className="flex w-4 h-4 p-10 whitespace-nowrap">
+            <div className="p-2">
+              <span className="block h-5/6 w-5/6 p-2 rounded-full border-[#3c50e0] border-8 bg-[#c3c8ee]"></span>
+            </div>
             <div className="w-full">
               <p className="font-semibold text-primary">Total Revenue</p>
               <p className="text-sm font-medium">12.04.2022 - 12.05.2022</p>
             </div>
           </div>
-          <div className="flex min-w-47.5">
-            <span className="mt-1 mr-2 flex h-4 w-full max-w-4 items-center justify-center rounded-full border border-secondary">
-              <span className="block h-2.5 w-full max-w-2.5 rounded-full bg-secondary"></span>
-            </span>
+          <div className="flex w-4 h-4 p-10 whitespace-nowrap">
+            <div className="p-2">
+              <span className="block h-5/6 w-5/6 p-2 rounded-full border-[#80caee] border-8 bg-[#cce4f0]"></span>
+            </div>
             <div className="w-full">
-              <p className="font-semibold text-secondary">Total Sales</p>
+              <p className="font-semibold text-primary">Total Sales</p>
               <p className="text-sm font-medium">12.04.2022 - 12.05.2022</p>
             </div>
           </div>
         </div>
-        <div className="flex w-full max-w-45 justify-end">
-          <div className="inline-flex items-center rounded-md bg-whiter p-1.5 dark:bg-meta-4">
-            <button className="rounded bg-white py-1 px-3 text-xs font-medium text-black shadow-card hover:bg-white hover:shadow-card dark:bg-boxdark dark:text-white dark:hover:bg-boxdark">
-              Day
-            </button>
-            <button className="rounded py-1 px-3 text-xs font-medium text-black hover:bg-white hover:shadow-card dark:text-white dark:hover:bg-boxdark">
-              Week
-            </button>
-            <button className="rounded py-1 px-3 text-xs font-medium text-black hover:bg-white hover:shadow-card dark:text-white dark:hover:bg-boxdark">
-              Month
-            </button>
-          </div>
-        </div>
-      </div>
-
-      <div>
-        <div id="chartOne" className="-ml-5">
+        <div>
           <ReactApexChart
-            options={options}
-            series={state.series}
-            type="area"
-            height={350}
-          />
+          options={options}
+          series={state.series}
+          type="area"
+          height={350}
+        />
         </div>
       </div>
     </div>
