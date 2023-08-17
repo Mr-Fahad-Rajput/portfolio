9220a63a02d4e75758f625ff3848fff99770c30c
Fahad Rajput <fahadameenrajput@gmail.com>
2023-08-17 07:24:22 +0500

    go

--- Floatfolio/frontend/src/pages/Dashboard/Dashboard.jsx
+++ Floatfolio/frontend/src/pages/Dashboard/Dashboard.jsx
@@ -9,11 +9,11 @@ function Dashboard() {
     return (
       <>
         <section className="mainContent">
-        <h1 className="mb-4 dark:text-secondaryBg font-semibold underline cursor-default text-balBrand border-y-2 dark:border-mainBg  border-dBrand">
+        <h1 className="mb-4 dark:text-secondaryBg font-semibold text-2xl underline cursor-default text-balBrand border-y-2 dark:border-mainBg  border-dBrand">
                 Monthly Report
         </h1>
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-8">
-        <CardOne cardImg={viewIcon} cardHeading="Views:353280" subHeading="Growth:" difPercntage="1.33%"/>
+        <div className="grid rounded-lg m-2 grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-8 bg-secondaryBg dark:bg-balBrand">
+        <CardOne cardImg={viewIcon} cardHeading="Visits:353280" subHeading="Growth:" difPercntage="1.33%"/>
         <CardOne cardImg={salesIcon} cardHeading="Sales: $25040" subHeading="Growth:" difPercntage="0.46%"/>
         <CardOne cardImg={profitIcon} cardHeading="Profits:$4560" subHeading="Growth:" difPercntage="2.89%"/>
         <CardOne cardImg={userIcon} cardHeading="Users:65490" subHeading="Growth:" difPercntage="10.09%"/>
--- Floatfolio/frontend/src/pages/components/CardOne.jsx
+++ Floatfolio/frontend/src/pages/components/CardOne.jsx
@@ -2,8 +2,8 @@ import up from '../../assets/up.svg'
 const CardOne = (props) => {
   const {cardImg, cardHeading, subHeading, difPercntage} = props;
   return (
-    <div className="rounded-lg m-4 border-2 border-dBrand dark:border-mainBg hover:scale-110 hover:bg-secondaryBg bg-lBrand p-4">
-      <div className="flex h-14 w-14 mx-auto overflow-hidden border-2 border-mainBg dark:border-dBrand items-center rounded-lg bg-balBrand dark:bg-mainBg">
+    <div className="rounded-lg m-4 border-2 border-dBrand dark:border-mainBg hover:scale-110 dark:bg-mainBg bg-lBrand p-4">
+      <div className="flex h-14 w-14 mx-auto overflow-hidden border-2 border-mainBg dark:border-dBrand items-center rounded-lg bg-balBrand dark:bg-secondaryBg">
         <img src={cardImg} alt="Card Image" className='object-cover'/>
       </div>
 
@@ -13,10 +13,10 @@ const CardOne = (props) => {
             {cardHeading}
           </h4>
         </div>
-        <span className="flex items-center gap-1 text-lg font-medium text-dBrand">
+        <span className="flex items-center gap-2 text-lg font-medium text-dBrand">
         <span className="text-lg font-medium">{subHeading}</span>
           {difPercntage}
-          <img src={up} alt="Up arrow Symbol" className='h-6 w-3'/>
+          <img src={up} alt="Up arrow Symbol" className='h-6 w-3 pb-1'/>
         </span>
 
       </div>
