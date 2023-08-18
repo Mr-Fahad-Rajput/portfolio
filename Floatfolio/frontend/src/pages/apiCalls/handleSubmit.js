cc2bb7be6baec19b857b5581bd15fa6483227873
Fahad Rajput <fahadameenrajput@gmail.com>
2023-08-18 20:14:17 +0500

    dynamic import function sign in INtegrated

---
+++ Floatfolio/frontend/src/pages/apiCalls/handleSubmit.js
@@ -0,0 +1,105 @@
+const handleSubmit = async (
+  event,
+  user,
+  type,
+  setErrStatus,
+  setResponseStatus,
+  setIsLoading,
+  navigate
+) => {
+  event.preventDefault();
+  let delay = 3000;
+  let endPoint, dataBody, successMsg;
+  setIsLoading(true);
+  if (type === "SignIn") {
+    const { email, password } = user;
+    dataBody = JSON.stringify({
+      email,
+      password,
+    });
+    endPoint = "login";
+    successMsg = "Login Succesful! Welcome aboard!";
+  } else if (type === "SignUp") {
+    const { username, email, password } = user;
+    dataBody = JSON.stringify({
+      username,
+      email,
+      password,
+    });
+    endPoint = "register";
+    successMsg = "Account Successfully Created! Welcome aboard!";
+  } else if (type === "msg") {
+    const { name, email, subject, message } = msg;
+    dataBody = JSON.stringify({
+      name,
+      email,
+      subject,
+      message,
+    });
+    endPoint = "message";
+    successMsg =
+      "Message Sent! Thanks for reaching out! We'll be in Touch Soon.";
+  }
+  try {
+    const res = await fetch("http://localhost:5000/" + endPoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: dataBody,
+    });
+    console.log(res.status);
+    if (res.status === 400 || !res) {
+      setErrStatus(true);
+      let errorMessage = await res.text();
+      console.error("Error:", errorMessage);
+      if (errorMessage.includes("duplicate")) {
+        errorMessage = "The Entered Details are Already Registered!";
+      }
+      errorMessage = errorMessage.replace(/\b\w+:|Path|(.,)\s*/g, "");
+      errorMessage = errorMessage.replace(/validation/g, "NOT SENT:");
+      errorMessage = errorMessage.replace(
+        /.*(?:mongodb\.net|ENOTFOUND).*$/g,
+        "Server Error: Database Server"
+      );
+      delay = 5000;
+      setResponseStatus({
+        status: true,
+        text: errorMessage,
+      });
+      setIsLoading(false);
+    } else {
+      setErrStatus(false);
+      setResponseStatus({
+        status: true,
+        text: successMsg,
+      });
+      setIsLoading(false);
+      delay = 2000;
+      setTimeout(() => {
+        navigate("/", { replace: true });
+      }, delay + 500);
+    }
+  } catch (error) {
+    setErrStatus(true);
+    console.log(error);
+    if (error.toString().includes("Failed to fetch")) {
+      delay = 4000;
+      setIsLoading(false);
+      setResponseStatus({
+        status: true,
+        text: "Can't Connect To the Server! Check Your Internet Connection",
+      });
+    }
+  }finally {
+    setTimeout(() => {
+      setErrStatus(false);
+      setResponseStatus({
+        status: false,
+        text: "",
+      });
+    }, delay);
+  }
+};
+
+export default handleSubmit;
--- Floatfolio/frontend/src/pages/components/SignIn.jsx
+++ Floatfolio/frontend/src/pages/components/SignIn.jsx
@@ -32,68 +32,23 @@ function SignIn() {
     setUser({ ...user, [name]: value });
   };
 
-  const handleSubmit = async (event) => {
+  const handleFormSubmit = async (event) => {
     event.preventDefault();
-    let delay = 3000;
-    setIsLoading(true);
-    const { email, password } = user;
     try {
-      const res = await fetch("http://localhost:5000/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email,
-          password,
-        }),
-      });
-      console.log(res.status);
-      if (res.status === 400 || !res) {
-        setErrStatus(true);
-        let errorMessage = await res.text();
-        console.error("Error:", errorMessage);
-        errorMessage = errorMessage.replace(
-          /.*(?:mongodb\.net|ENOTFOUND).*$/g,
-          "Server Error: Database Server"
-        );
-        delay = 5000;
-        setResponseStatus({
-          status: true,
-          text: errorMessage,
-        });
-        setIsLoading(false);
-      } else {
-        setErrStatus(false);
-        setResponseStatus({
-          status: true,
-          text: "Login Succesful! Welcome aboard!",
-        });
-        setIsLoading(false);
-        delay = 2000;
-        setTimeout(() => {
-          navigate("/", { replace: true });
-        }, delay + 500);
-      }
+      const handleSubmitModule = await import('../apiCalls/handleSubmit.js');
+      const handleSubmit = handleSubmitModule.default;
+      handleSubmit(
+        event,
+        user,
+        'SignIn',
+        setErrStatus,
+        setResponseStatus,
+        setIsLoading,
+        navigate
+      );
     } catch (error) {
-      setErrStatus(true);
-      console.log(error);
-      if (error.toString().includes("Failed to fetch")) {
-        delay = 4000;
-        setResponseStatus({
-          status: true,
-          text: "Can't Connect To the Server! Check Your Internet Connection",
-        });
-      }
-    } finally {
-      setTimeout(() => {
-        setErrStatus(false);
-        setResponseStatus({
-          status: false,
-          text: "",
-        });
-      }, delay);
-    }
+      console.error('Error importing handleSubmit:', error);
+    } 
   };
 
   // Front End Navigation
@@ -120,7 +75,7 @@ function SignIn() {
               <h5 className="mb-2 text-xl dark:text-mainBg font-semibold underline cursor-default w-[98%]">
                 Login
               </h5>
-              <form onSubmit={handleSubmit}>
+              <form onSubmit={handleFormSubmit}>
                 <div className="grid grid-cols-1">
                   <div className="mb-4">
                     <label
