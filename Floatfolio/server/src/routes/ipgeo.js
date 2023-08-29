28f9aceb01b8318e134c223525fd02f4ac20aeb1
Fahad Rajput <fahadameenrajput@gmail.com>
2023-08-29 23:19:24 +0500

    Weather API Working

--- Floatfolio/server/src/routes/ipgeo.js
+++ Floatfolio/server/src/routes/ipgeo.js
@@ -3,12 +3,32 @@ module.exports = async (req, res) => {
   try {
     const ip = req.body.ip;
     const response = await axios.get(`https://ipinfo.io/${ip}/json`);
-    const data = response.data;
-    console.log("Geolocation Data:", data);
-    
-    res.status(200).json(data);
+    const geoData = response.data;
+    console.log("Geolocation geoData:", geoData);
+    const location = geoData.loc;
+    const city = geoData.city;
+    const region = geoData.region;
+    const country = geoData.country
+    console.log(location,city,country,region)
+
+    const options = {
+        method: 'GET',
+        url: 'https://weatherapi-com.p.rapidapi.com/current.json',
+        params: {q: location},
+        headers: {
+          'X-RapidAPI-Key': process.env.XGEO_KEY,
+          'X-RapidAPI-Host': process.env.XGEO_HOST
+        }
+      };
+      
+      try {
+          const response = await axios.request(options);
+          console.log(response.data);
+      } catch (error) {
+          console.error(error);
+      }
   } catch (error) {
-    console.error("Error fetching geolocation data:", error);
+    console.error("Error fetching geolocation geoData:", error);
     const errorMessage = error.message;
     res.status(500).send(errorMessage);
   }
