b7976a9304b96d5fdd96e594507b97b85b185716
Fahad Rajput <fahadameenrajput@gmail.com>
2023-08-30 23:52:05 +0500

    go

--- Floatfolio/frontend/src/pages/APIs/IPGeo/IPGeo.jsx
+++ Floatfolio/frontend/src/pages/APIs/IPGeo/IPGeo.jsx
@@ -299,56 +299,54 @@ function IPGeo() {
             How To:
           </h3>
           <p className=" mt-3 mx-auto text-justify">Just a Test</p>
+          <button
+            name="send"
+            className="p-4 m-1 btn whitespace-nowrap mx-auto"
+            onClick={() => {
+              setFetchData(!fetchData);
+            }}
+            disabled={isLoading}
+            aria-label="Check Out Button"
+          >
+            {isLoading ? (
+              <div className="flex items-center">
+                <div className="animate-spin mr-2">
+                  <svg
+                    className="w-5 h-5 text-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-100 "
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="#FEFAE6"
+                      strokeWidth="4"
+                    ></circle>
+                    <path
+                      className="opacity-75"
+                      fill="#471AA0"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.963 7.963 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    ></path>
+                  </svg>
+                </div>
+                Fetching Api...
+              </div>
+            ) : (
+              "Fetch Weather"
+            )}
+          </button>
         </div>
         <div className="p-2 w-full">
           {!location ? (
             <>
-              <button
-                name="send"
-                className="p-4 m-1 btn whitespace-nowrap mx-auto"
-                onClick={() => {
-                  setFetchData(!fetchData);
-                }}
-                disabled={isLoading}
-                aria-label="Check Out Button"
-              >
-                {isLoading ? (
-                  <div className="flex items-center">
-                    <div className="animate-spin mr-2">
-                      <svg
-                        className="w-5 h-5 text-white"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                      >
-                        <circle
-                          className="opacity-100 "
-                          cx="12"
-                          cy="12"
-                          r="10"
-                          stroke="#FEFAE6"
-                          strokeWidth="4"
-                        ></circle>
-                        <path
-                          className="opacity-75"
-                          fill="#471AA0"
-                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.963 7.963 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                        ></path>
-                      </svg>
-                    </div>
-                    Fetching Api...
-                  </div>
-                ) : (
-                  "FetchData Weather"
-                )}
-              </button>
               <div className="flex h-screen items-center justify-center w-full">
                 <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-balBrand dark:border-mainBg border-t-transparent"></div>
               </div>
             </>
           ) : (
-           
-            
             setConditionImage(current.condition.text) && (
               <>
                 <div className=" text-dBrand text-2xl break-words rounded-lg overflow-hidden mb-4 bg-lBrand dark:bg-balBrand border-2 border-dBrand dark:border-mainBg animate-scale-100 duration-75">
