b18fb5002f4a00a0a5440d1a126d6e2e3c3496c8
Fahad Rajput <fahadameenrajput@gmail.com>
2023-09-19 21:46:54 +0500

    Srever Response okay

--- Floatfolio/server/src/routes/whisper.js
+++ Floatfolio/server/src/routes/whisper.js
@@ -1,39 +1,39 @@
 const { OpenAI } = require("openai");
-const axios = require('axios');
-const fs = require('fs');
-const path = require('path');
-const FormData = require('form-data');
+const fs = require("fs");
+const { Readable } = require('stream');
+
 
 module.exports = async (req, res) => {
-    // const apiKey = process.env.OPEN_AI_SECRET;
-    // con
   try {
-    // if (!req.file) {
-    //     res.status(400).json({ error: "No image file uploaded." });
-    //     return;
-    //   }
-    //   const audioContent = req.file;
-      const openai = new OpenAI({
-          apiKey: process.env.OPEN_AI_SECRET,
-        });
-        const image = fs.createReadStream('audio.mp3');
-        console.log(image)
-        // const userMessage = req.body.message;
-        // const userContext = req.body.status;
-        // console.log("Context:"+ userContext,"Message:"+ userMessage);
-        
-        try {
-        // const response = await openai.audio.transcriptions.create({
-        //     model: 'whisper-1',
-        //     file: audioContent.buffer,
-        //   });
-        const response = await openai.audio.transcriptions.create({
-            model: 'whisper-1',
-            file: fs.createReadStream('audio.mp3'),
-          });
-          
-          console.log(response)
-      res.json({ response});
+    if (!req.file) {
+        res.status(400).json({ error: "No Audio file uploaded." });
+        return;
+      }
+      fs.writeFileSync(__dirname +"/../routes/"+req.file.originalname, req.file.buffer);
+      var audioReadStream = fs.createReadStream(__dirname +"/../routes/"+req.file.originalname);
+    //   const audioReadStream = Readable.from(req.file.buffer);
+    //   audioReadStream.path = "audio.mp3";
+
+
+      console.log("Test:"+JSON.stringify(audioReadStream))
+    const openai = new OpenAI({
+      apiKey: process.env.OPEN_AI_SECRET,
+    });
+    const image = fs.createReadStream("audio.mp3");
+    console.log("image:"+JSON.stringify(image))
+    // const userMessage = req.body.message;
+    // const userContext = req.body.status;
+    // console.log("Context:"+ userContext,"Message:"+ userMessage);
+
+    try {
+      const response = await openai.audio.transcriptions.create({
+        model: "whisper-1",
+        file: audioReadStream
+        // file: image
+      });
+
+      console.log(response);
+      res.json({ response });
     } catch (error) {
       console.error("Error:", error);
       res.status(500).json({ error: "An error occurred" });
