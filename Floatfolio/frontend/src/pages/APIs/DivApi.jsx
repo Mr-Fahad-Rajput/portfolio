fa63e61916af966c37953a2e45bc56c07e716402
Fahad Rajput <fahadameenrajput@gmail.com>
2023-09-21 13:28:02 +0500

    APIS Done and dusted with 9 APIs.

--- Floatfolio/frontend/src/pages/APIs/DivApi.jsx
+++ Floatfolio/frontend/src/pages/APIs/DivApi.jsx
@@ -1,31 +1,41 @@
 import { useNavigate } from "react-router-dom";
 
 const DivApi = (props) => {
-    const navigate = useNavigate();
-    // eslint-disable-next-line 
+  const navigate = useNavigate();
+  // eslint-disable-next-line
   const { divImg, divHeading, description, difficulty, route } = props;
   return (
-    <div onClick={()=>{navigate(route) } } className=" rounded-lg m-2 border-2 border-dBrand dark:bg-mainBg hover:scale-110 bg-lBrand p-2 transform duration-500 ">
+    <div
+      onClick={() => {
+        navigate(route);
+      }}
+      className=" rounded-lg m-2 border-2 border-dBrand dark:bg-mainBg hover:scale-110 bg-lBrand p-2 transform duration-500"
+    >
       <img
         className="rounded-lg h-[170px] m-auto"
         src={divImg}
         alt="API Image"
-        loading = "lazy"
+        loading="lazy"
       />
       <div className="p-2">
         <h5 className=" text-2xl font-bold text-balBrand tracking-tight mb-2 text-center">
           {divHeading}
         </h5>
-        {/* TODO manage word SPacing issue/ Kind of okay but check with the rest of the App */}
-        <p className="text-base font-medium text-dBrand break-words text-justify tracking-tighter [word-spacing:-3px]">
+        <p className="text-base font-medium text-dBrand break-words text-justify tracking-tighter mb-10">
           {description}
         </p>
-        <div className="m-auto h-auto w-full ">
-          <div>
-            <h4 className=" text-xl dark:text-dBrand font-semibold underline cursor-default">Integration Complexity</h4>
-          </div>
-          <div className="w-full bg-mainBg dark:bg-lBrand outline-2 outline outline-dBrand  h-5 mt-2 rounded">
-            <div className={` bg-mainBg h-5 rounded ${difficulty} text-dBrand text-center`}><p>{difficulty}</p></div>
+      </div>
+      <div className=" m-2 self-center bottom-0 w-11/12 absolute" id="bar">
+        <div>
+          <h4 className=" text-xl dark:text-dBrand font-semibold underline cursor-default">
+            Integration Complexity
+          </h4>
+        </div>
+        <div className="w-full bg-mainBg dark:bg-lBrand outline-2 outline outline-dBrand  h-5 mt-2 rounded">
+          <div
+            className={` bg-mainBg h-5 rounded ${difficulty} text-dBrand text-center`}
+          >
+            <p className="my- font-semibold">{difficulty}</p>
           </div>
         </div>
       </div>
