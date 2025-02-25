1188e1e5995ff947f976141e3007776ba1e40c70
Mr-Fahad-Rajput <fahadameenrajput@gmail.com>
2025-02-25 20:25:01 +0500

    Mail chimp error resolved

--- Floatfolio/server/src/routes/mailchimp.js
+++ Floatfolio/server/src/routes/mailchimp.js
@@ -1,42 +1,63 @@
-const request = require('request')
+const request = require('request');
+const Buffer = require('buffer').Buffer;
+
 module.exports = async (req, res) => {
     try {
-      const email = req.body.email
-      const reqStatus =req.body.status;
-      console.log(reqStatus)
-      const mcData = {
-        members: [{
-            email_address: email,
-            status: reqStatus
-        }]
-      }
-      const dataPost = JSON.stringify(mcData);
-      const options = {
-        url: process.env.MC_URL,
-        method: 'POST',
-        headers: {
-            Authorization: process.env.MC_API_KEY
-        },  
-        body: dataPost
-      }
-      if(email){
-        var setUrl = "";
-        request(options,(err,response, body)=>{
-            if(err){
-                setUrl = `${process.env.SERVER_URL}/mailchimp#cancel`,
-                res.status(400).json({error:err}).send({message:"Error Occured", urlRes: setUrl})
-            }else{
-                setUrl=`${process.env.SERVER_URL}/mailchimp#success`,
-                res.status(200).send({ message: "Successful", urlRes: setUrl });
+        const email = req.body.email;
+        const reqStatus = req.body.status;
+
+        if (!email) {
+            return res.status(400).send("Email is required");
+        }
+
+        const mcData = {
+            members: [{
+                email_address: email,
+                status: reqStatus
+            }]
+        };
+
+        // Generate Basic Auth header
+        const apiKey = process.env.MC_API_KEY;
+        const authHeader = 'Basic ' + Buffer.from(`apikey:${apiKey}`).toString('base64');
+
+        const options = {
+            url: process.env.MC_URL,
+            method: 'POST',
+            headers: {
+                Authorization: authHeader,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(mcData)
+        };
+
+        request(options, (err, response, body) => {
+            if (err) {
+                console.error('Request error:', err);
+                const setUrl = `${process.env.SERVER_URL}/mailchimp#cancel`;
+                return res.status(500).send({ message: "Request failed", urlRes: setUrl });
             }
-        })
-      }else{
-        res.status(404).send("Email Not Found")
-      }
+            console.log(response.body)
+            if (response.statusCode === 200) {
+                const setUrl = `${process.env.SERVER_URL}/mailchimp#success`;
+                return res.status(200).send({ message: "Successful", urlRes: setUrl });
+            } else {
+                console.error('Mailchimp error:', body);
+                const setUrl = `${process.env.SERVER_URL}/mailchimp#cancel`;
+                return res.status(400).send({ 
+                    message: "Error occurred", 
+                    error: JSON.parse(body),
+                    urlRes: setUrl
+                });
+            }
+        });
+
     } catch (error) {
-      console.log(error);
-      const errorMessage = error.message;
-      res.status(500).send(errorMessage);
+        console.error('Server error:', error);
+        const setUrl = `${process.env.SERVER_URL}/mailchimp#cancel`;
+        return res.status(500).send({ 
+            message: error.message,
+            urlRes: setUrl
+        });
     }
-  };
-  
+};
